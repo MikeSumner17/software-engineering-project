5e6ff749ee5133d75ab4ee7b977c1c087c56e67a
Brian Blazek <98470557+BlazekBrian@users.noreply.github.com>
2022-03-23 19:14:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const createAccountButton = document.getElementById("create-account-form");
+const createAccountButton = document.getElementById("create-account-button");
 
-loginButton.addEventListener("click", (e) => {
+createAccountButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
-        window.location.href = "main.html";
-    } else {
-        alert("Invalid username and/or password.");
-    }
-})
-
-createAccountButton.addEventListener("click", e => {
-    e.preventDefault();
+    window.location.href = "createaccount.html";
 })
