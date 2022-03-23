547a1f17dd2d8bdcf1719e8e0cddb0a9a8ac6c29
Brian Blazek <98470557+BlazekBrian@users.noreply.github.com>
2022-03-23 19:30:11 -0400

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -9,45 +9,46 @@ DEFINE('DB_NAME', 'Team5GymManagementDB');
 // Create connection
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-if ( !isset($_POST['username'], $_POST['password']) ) {
+// Now we check if the data was submitted, isset() function will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'])) {
 	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+	exit('Please complete the registration form!');
+}
+// Make sure the submitted registration values are not empty.
+if (empty($_POST['username']) || empty($_POST['password'])) {
+	// One or more values are empty.
+	exit('Please complete the registration form');
 }
 
+// We need to check if the account with that username exists.
 if ($stmt = $con->prepare('SELECT password FROM users WHERE user_name = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
+	// Store the result so we can check if the account exists in the database.
+	if ($stmt->num_rows > 0) {
+		// Username already exists
+        ?><script> alert("Username exists, please choose another!"); window.history.back();</script><?php
 
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($password);
-        $stmt->fetch();
-        // Account exists, now we verify the password.
-        // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if ($_POST['password'] === $password) {            
-            // Verification success! User has logged-in!
-            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['name'] = $_POST['username'];
-            $_SESSION['id'] = $id;
-            header('Location: main.php');
+	} else {
+		// Username doesnt exists, insert new account
+        if ($stmt = $con->prepare('INSERT INTO users (user_name, password) VALUES (?, ?)')) {
+	    // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+	        //$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	        $stmt->bind_param('ss', $_POST['username'], $_POST['password']);
+	        $stmt->execute();
+            header('Location: index.html');
         } else {
-            // Incorrect password
-            ?><script> alert("Incorrect Username and/or Password"); window.history.back();</script><?php
+	        // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+            ?><script> alert("Could not prepare statement!"); window.history.back();</script><?php
         }
-    } else {
-        // Incorrect username
-        ?><script> alert("Incorrect Username and/or Password"); window.history.back();</script><?php
-    }
-
+	}
 	$stmt->close();
+} else {
+	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+    ?><script> alert("Could not prepare statement!"); window.history.back();</script><?php
+
 }
+$con->close();
 ?>
