ff92ec8f12a9720ea4dfb80586de6ff14b14130c
RyanAtRowan <91160247+RyanAtRowan@users.noreply.github.com>
2022-04-20 22:47:07 -0400

    Add files via upload

---
+++ authenticate.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+DEFINE('DB_HOST', 'gymmanagementdb.cvouqioew9pk.us-east-1.rds.amazonaws.com');
+DEFINE('DB_USER', 'team5');
+DEFINE('DB_PASSWORD', 'team5sweng');
+DEFINE('DB_NAME', 'Team5GymManagementDB');
+
+// Create connection
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
+
+if ($stmt = $con->prepare('SELECT password FROM users WHERE user_name = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if ($_POST['password'] === $password) {            
+            // Verification success! User has logged-in!
+            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            header("Location: main/main.html");
+        } else {
+            // Incorrect password
+            ?><script> alert("Incorrect Username and/or Password"); window.history.back();</script><?php
+        }
+    } else {
+        // Incorrect username
+        ?><script> alert("Incorrect Username and/or Password"); window.history.back();</script><?php
+    }
+
+	$stmt->close();
+}
+?>
---
+++ index.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const createAccountButton = document.getElementById("create-account-button");
+
+createAccountButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = ('create_account/createaccount.html');
+})
